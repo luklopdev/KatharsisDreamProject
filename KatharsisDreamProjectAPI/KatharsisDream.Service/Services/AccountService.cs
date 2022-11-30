caef3a944d30fca828811bf01548611603937b90
Łukasz Łopata <lukasz.lopata.developer@gmail.com>
2022-11-30 22:32:02 +0100

    Impemented Database Context, created Service and Repository modules, added initial account services, models and repositories

---
+++ KatharsisDreamProjectAPI/KatharsisDream.Service/Services/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatharsisDream.Service.Services
+{
+    public interface IAccountService
+    {
+
+    }
+
+    public class AccountService
+    {
+
+    }
+}
---
+++ KatharsisDreamProjectAPI/KatharsisDreamProjectAPI/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using KatharsisDream.Service.DTO.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KatharsisDreamProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("Register")]
+        public async Task<ActionResult> RegisterAccount([FromBody] RegisterAccountDto registerAccountDto)
+        {
+            return Ok();
+        }
+    }
+}
